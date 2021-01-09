7b87849971be8d0fb5159587c13fce7793af3217
majhar-nayem <majharul.islam02@northsouth.edu>
2021-01-09 18:10:38 +0600

    fillable array created

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        "name",
+        "area",
+        "address",
+        "type",
+        "ratings",
+        "note"
+    ];
 }
