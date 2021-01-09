4177bee01e5233eb46b50c7d9d9a076b028ae929
majhar-nayem <majharul.islam02@northsouth.edu>
2021-01-09 18:09:01 +0600

    model and migration created

---
+++ database/migrations/2021_01_09_120211_create_restaurents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('area');
+            $table->string('address');
+            $table->string('type')->nullable();
+            $table->string('ratings')->default('0');
+            $table->string('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurents');
+    }
+}
